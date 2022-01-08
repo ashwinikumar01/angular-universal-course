9fcca4ec9ab7cdd95bd5b6f1acdb9fac8693a710
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2022-01-08 22:11:13 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: "home",
     component: HomeComponent
 
   },
@@ -24,7 +24,7 @@ const routes: Routes = [
   },
   {
     path: "**",
-    redirectTo: '/'
+    redirectTo: 'home'
   }
 ];
 
